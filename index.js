9faad7e2d57be4a3ad0db4e1a735cb69734c2cb0
YuJi-2023 <sophia.jiyu@gmail.com>
2024-11-02 11:55:18 +1100

    create schema, define usertype, rootquery and mutation

--- index.js
+++ index.js
@@ -3,13 +3,77 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const userData = require("./MOCK_DATA.json");
 const graphql = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLList,
+} = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
 
+//create user type
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLInt },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+  }),
+});
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}))
+//create query and mutation viarables for creating types
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    getAllUsers: {
+      type: new GraphQLList(UserType),
+      args: { id: { type: GraphQLInt } },
+      resolve(parent, args) {
+        return userData;
+      },
+    },
+  },
+});
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: UserType,
+      args: {
+        firstName: { type: GraphQLString },
+        lastName: { type: GraphQLString },
+        email: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      resolve(parent, args) {
+        userData.push({
+          id: userData.length + 1,
+          firstName: args.firstName,
+          lastName: args.lastName,
+          email: args.email,
+          password: args.password,
+        });
+        return args;
+      },
+    },
+  },
+});
+
+//create schema
+const schema = new graphql.GraphQLSchema({
+  query: RootQuery,
+  mutation: Mutation,
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
