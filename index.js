8acbe27b1ac860bd52fa97110ed3419e0b71ef90
YuJi-2023 <sophia.jiyu@gmail.com>
2024-11-02 11:17:16 +1100

    set up api server

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const userData = require("./MOCK_DATA.json");
+const graphql = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
 
-// app.get("/", (req, res) => {
-//   res.send("Hello, World!");
-// });
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
