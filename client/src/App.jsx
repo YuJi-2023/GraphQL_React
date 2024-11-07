32ec362f4afbc349627f7ce9e017f07bac9158b6
YuJi-2023 <sophia.jiyu@gmail.com>
2024-11-07 21:43:53 +1100

    install apolloclient and graphql dependencies create client instance and wrap app with apolloprovider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink, from } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+
+// create an instance of Apollo client
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:3000/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>My GraphQL + React</h1>
-    </>
+    //wrap app with ApolloProvider and pass in the previously defined client 
+    <ApolloProvider client={client}></ApolloProvider>
   )
 }
 
